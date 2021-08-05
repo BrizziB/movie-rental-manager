befb308e8f79802d243beba3463bbc45fce67ee6
MarcoGeri <mrc.geri@gmail.com>
2021-08-05 16:40:21 +0200

    created BaseEntity

---
+++ src/main/java/it/unifi/ing/stlab/movierentalmanager/model/BaseEntity.java
@@ -0,0 +1,51 @@
+package it.unifi.ing.stlab.movierentalmanager.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue
+    private String id;
+    private UUID uuid;
+    private Timestamp creationTime;
+    private Timestamp lastUpdateTime;
+
+    public BaseEntity() {
+
+    }
+
+    public BaseEntity(UUID uuid) {
+        this.uuid = uuid;
+        this.creationTime = new Timestamp(System.currentTimeMillis());
+        this.lastUpdateTime = new Timestamp(System.currentTimeMillis());
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public Timestamp getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(Timestamp creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public Timestamp getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(Timestamp lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+}
--- src/main/java/it/unifi/ing/stlab/movierentalmanager/model/Movie.java
+++ src/main/java/it/unifi/ing/stlab/movierentalmanager/model/Movie.java
@@ -1,14 +1,17 @@
 package it.unifi.ing.stlab.movierentalmanager.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "movies")
-public class Movie {
+public class Movie extends BaseEntity {
     @Id
+    @GeneratedValue
     private String id;
     private String title;
+
 
 }
